66a55e0bf4ff244c870c9bb64140873e06ac4761
William Cui <444738577@qq.com>
2020-09-03 09:51:20 +0800

    delete .umi folder

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/williamcui/.nvm/versions/node/v14.4.0/lib/node_modules/dumi/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange',],
-});
-
-export { plugin };
