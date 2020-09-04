6c7c42f78644328a7bf6bf337c4ac06aa1cec2ff
William Cui <444738577@qq.com>
2020-09-04 17:50:06 +0800

    打包声明类型文件

---
+++ lib/button/button.d.ts
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+import { ButtonProps } from './types';
+/**
+ *
+ * @param props  any
+ * @returns void
+ */
+declare const Button: FC<ButtonProps>;
+export default Button;
