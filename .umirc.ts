d002c892fe449ceb77acb1c74e6a51c8b7766d94
William Cui <444738577@qq.com>
2020-09-09 14:58:58 +0800

    修改readme文件

--- .umirc.ts
+++ .umirc.ts
@@ -6,13 +6,11 @@ export default {
       'import',
       {
         libraryName: 'iuc',
-        // camel2DashComponentName: false,
-        // camel2UnderlineComponentName: false,
         libraryDirectory: "lib",
         customStyleName: name => {
-          return `./style/index.less`; // 注意：这里 ./ 不可省略
+          return `./style`; // 注意：这里 ./ 不可省略
         },
-      }
+      },
     ],
   ],
   base: '/iuc',
